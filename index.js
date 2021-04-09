cfce3a47d1d401cfb30c112dffcbee88f3c07d08
Andy Schachter <andy.schachter@gmail.com>
2021-04-08 23:29:03 -0400

    updated if statements for null replace, added if statement to return NaN if no operator

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+/* eslint-disable no-console */
 /* eslint-disable eqeqeq */
 const calculate = (arr) => {
+  let operatorArr = ['*', '/', '+', '-']
+
+  const operCheck = (operator) => operatorArr.includes(operator)
+
   let calcArr = arr.filter(inputs => {
-    return inputs != undefined && inputs != ''
+    return inputs != undefined || inputs != ''
   })
 
-  for (let i = 0; i < arr.length; i++) {
-    let operatorArr = ['*', '/', '+', '-']
-
-    if (operatorArr.includes(arr[i]) === false && isNaN(arr[i]) === true) {
+  for (let i = 0; i < calcArr.length; i++) {
+    if (operatorArr.includes(calcArr[i]) === false && isNaN(calcArr[i]) === true) {
       calcArr.splice(i, 1)
+    }
+    if (calcArr[i] === null) {
+      calcArr.splice(i, 1, 0)
     }
   }
 
-  return eval(calcArr.join(' '))
+  if (calcArr.some(operCheck)) {
+    return eval(calcArr.join(''))
+  } else return NaN
 }
 
-// eslint-disable-next-line no-console
-console.log(calculate(['6', '*', 'foo', 2]))
+
+// eslint-disable-next-line no-sparse-arrays
+console.log(calculate(['6', '', '*', 'bar', , null]))
 
 module.exports = calculate
